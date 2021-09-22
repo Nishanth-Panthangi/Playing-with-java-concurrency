a8804e815c8f75206e817faecb66a41c560902ac
Nishanth Panthangi <nishanthkwel@gmail.com>
2021-09-22 22:27:27 +0530

    Update RunnableExampleLambdaExpression.java

--- java-thread-and-runnable/src/RunnableExampleLambdaExpression.java
+++ java-thread-and-runnable/src/RunnableExampleLambdaExpression.java
@@ -10,5 +10,8 @@ public class RunnableExampleLambdaExpression {
 
         System.out.println("Creating Thread...");
         Thread thread = new Thread(runnable);
+        
+        System.out.println("Starting Thread...");
+        thread.start();
     }
 }
