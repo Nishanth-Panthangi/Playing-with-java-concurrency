fc2750c0c94246642858b97220b86435262c8757
Nishanth Panthangi <nishanthkwel@gmail.com>
2021-09-22 22:31:05 +0530

    Update FutureAndCallableExample.java

--- java-callable-and-future/src/FutureAndCallableExample.java
+++ java-callable-and-future/src/FutureAndCallableExample.java
@@ -13,4 +13,13 @@ public class FutureAndCallableExample {
             Thread.sleep(2000);
             return "Hello from Callable";
         };
+	System.out.println("Submitting Callable");	
+        Future<String> future = executorService.submit(callable);
+        // This line executes immediately
+        System.out.println("Do something else while callable is getting executed");
+
+        System.out.println("Retrieve the result of the future");
+        // Future.get() blocks until the result is available
+        
+    }
 }
